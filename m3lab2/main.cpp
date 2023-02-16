5e8a2a8a088663731aa05f185fa0c3f2a1b4297f
yogurttube <bythegraceofdogs@gmail.com>
2023-02-16 11:25:28 -0500

    Update main.cpp

--- m3lab2/main.cpp
+++ m3lab2/main.cpp
@@ -1,9 +1,100 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+// csc249
+// m3lab2
+// 2-16-23
+
+void Merge(int* numbers, int leftFirst, int leftLast, int rightLast) {
+   int mergedSize = rightLast - leftFirst + 1;       // Size of merged partition
+   int* mergedNumbers = new int[mergedSize]; // Dynamically allocates temporary
+                                             // array for merged numbers
+   int mergePos = 0;                         // Position to insert merged number
+   int leftPos = leftFirst;                  // Initialize left partition position
+   int rightPos = leftLast + 1;              // Initialize right partition position
+
+   // Add smallest element from left or right partition to merged numbers
+   while (leftPos <= leftLast && rightPos <= rightLast) {
+      if (numbers[leftPos] <= numbers[rightPos]) {
+         mergedNumbers[mergePos] = numbers[leftPos];
+         leftPos++;
+      }
+      else {
+         mergedNumbers[mergePos] = numbers[rightPos];
+         rightPos++;
+      }
+      mergePos++;
+   }
+
+   // If left partition is not empty, add remaining elements to merged numbers
+   while (leftPos <= leftLast) {
+      mergedNumbers[mergePos] = numbers[leftPos];
+      leftPos++;
+      mergePos++;
+   }
+
+   // If right partition is not empty, add remaining elements to merged numbers
+   while (rightPos <= rightLast) {
+      mergedNumbers[mergePos] = numbers[rightPos];
+      rightPos++;
+      mergePos++;
+   }
+
+   // Copy merged numbers back to numbers
+   for (mergePos = 0; mergePos < mergedSize; mergePos++) {
+      numbers[leftFirst + mergePos] = mergedNumbers[mergePos];
+   }
+
+   // Free temporary array
+   delete[] mergedNumbers;
+}
+
+void MergeSort(int* numbers, int startIndex, int endIndex) {
+   if (startIndex < endIndex) {
+      // Find the midpoint in the partition
+      int mid = (startIndex + endIndex) / 2;
+
+      // Recursively sort left and right partitions
+      MergeSort(numbers, startIndex, mid);
+      MergeSort(numbers, mid + 1, endIndex);
+
+      // Merge left and right partition in sorted order
+      Merge(numbers, startIndex, mid, endIndex);
+   }
+}
+
+string ArrayToString(int* array, int arraySize) {
+   // Special case for empty array
+   if (0 == arraySize) {
+      return string("[]");
+   }
+
+   // Start the string with the opening '[' and element 0
+   string result = "[" + to_string(array[0]);
+
+   // For each remaining element, append comma, space, and element
+   for (int i = 1; i < arraySize; i++) {
+      result += ", ";
+      result += to_string(array[i]);
+   }
+
+   // Add closing ']' and return result
+   result += "]";
+   return result;
+}
+
+int main() {
+   // Create an array of numbers to sort
+   int numbers[] = { 61, 76, 19, 4, 94, 32, 27, 83, 58 };
+   int numbersSize = sizeof(numbers) / sizeof(numbers[0]);
+
+   // Display the contents of the array
+   cout << "UNSORTED: " << ArrayToString(numbers, numbersSize) << endl;
+
+   // Call the MergeSort function
+   MergeSort(numbers, 0, numbersSize - 1);
+
+   // Display the sorted contents of the array
+   cout << "SORTED:   " << ArrayToString(numbers, numbersSize) << endl;
 }
